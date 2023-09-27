42c0bfa627d953c2aed613f3e6294315a4d05337
Aylin Us <Aylinus9696@hotmail.com>
2023-09-27 19:17:48 +0300

    aa

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        
+
+    }
+}
+
---
+++ Repository/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        DbSet<T> Set();
+        T Add(T entity);
+        T Update(T entity);
+        T Delete(T entity);
+        T Find(int id);
+        List<T> List();
+        T GetFirst(Expression<Func<T, bool>> whereCondition);
+        IQueryable<T> Get(Expression<Func<T, bool>> whereCondition);
+    }
+}
